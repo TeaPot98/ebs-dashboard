16b132625992f29cc328ccd4de4054aead2a7677
TeaPot98 <trifautan@gmail.com>
2022-09-22 16:45:31 +0300

    removed try-catch blocks from posts.ts and users.ts

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -3,52 +3,30 @@ import axios from "./axios";
 import models from "models";
 
 export const create = async (post: models.Post) => {
-  try {
-    const response = await axios.post<models.Post>(`/posts`, post);
-
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.post<models.Post>(`/posts`, post);
+  return response.data;
 };
 
 export const getById = async (postId: string) => {
-  try {
-    const response = await axios.get<models.Post>(`/posts/${postId}`);
-    return { ...response.data, date: new Date(response.data.date) };
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get<models.Post>(`/posts/${postId}`);
+  return { ...response.data, date: new Date(response.data.date) };
 };
 
 export const getAll = async () => {
-  try {
-    const response = await axios.get<models.Post[]>(`/posts`);
-
-    // Parse date and return
-    return response.data.map((post) => ({
-      ...post,
-      date: new Date(post.date),
-    }));
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get<models.Post[]>(`/posts`);
+  // Parse date and return
+  return response.data.map((post) => ({
+    ...post,
+    date: new Date(post.date),
+  }));
 };
 
 export const edit = async (post: models.Post) => {
-  try {
-    const response = await axios.put(`/posts/${post.id}`, post);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.put(`/posts/${post.id}`, post);
+  return response.data;
 };
 
 export const remove = async (postId: string) => {
-  try {
-    const response = await axios.delete(`/posts/${postId}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.delete(`/posts/${postId}`);
+  return response.data;
 };
--- src/api/users.ts
+++ src/api/users.ts
@@ -3,70 +3,45 @@ import axios from "./axios";
 import models from "models";
 
 export const register = async (user: models.UserRegistration) => {
-  try {
-    // Check if user already exists
-    const response = await axios.get<models.User[]>(
-      `/users?email=${user.email}`
-    );
-    if (response.data.length > 0) {
-      throw new Error("The user with this email already exists");
-    }
-    // Register new user
-    const registeredUser = await axios.post<models.User>(`/users`, user);
-    return registeredUser.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
+  // Check if user already exists
+  const response = await axios.get<models.User[]>(`/users?email=${user.email}`);
+  if (response.data.length > 0) {
+    throw new Error("The user with this email already exists");
   }
+  // Register new user
+  const registeredUser = await axios.post<models.User>(`/users`, user);
+  return registeredUser.data;
 };
 
 export const edit = async (user: models.User) => {
-  try {
-    const response = await axios.put(`/users/${user.id}`, user);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.put(`/users/${user.id}`, user);
+  return response.data;
 };
 
 export const remove = async (userId: string) => {
-  try {
-    const response = await axios.delete(`/users/${userId}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.delete(`/users/${userId}`);
+  return response.data;
 };
 
 export const login = async (userCredentials: models.UserCredentials) => {
-  try {
-    const response = await axios.get<models.User[]>(
-      `/users?email=${userCredentials.email}&password=${userCredentials.password}`
-    );
-    if (response.data.length === 0) {
-      throw new Error("Incorrect email or password!");
-    }
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    throw error;
+  const response = await axios.get<models.User[]>(
+    `/users?email=${userCredentials.email}&password=${userCredentials.password}`
+  );
+
+  if (response.data.length === 0) {
+    throw new Error("Incorrect email or password!");
   }
+
+  console.log(response.data);
+  return response.data;
 };
 
 export const getById = async (userId: string) => {
-  try {
-    const response = await axios.get<models.User>(`/users/${userId}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get<models.User>(`/users/${userId}`);
+  return response.data;
 };
 
 export const getAll = async () => {
-  try {
-    const response = await axios.get<models.User[]>(`/users`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get<models.User[]>(`/users`);
+  return response.data;
 };
