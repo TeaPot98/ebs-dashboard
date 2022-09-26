b58adb73c9089df7853ca7fea8a4f483a94395d7
Vlad Trifautan <trifautan@gmail.com>
2022-09-26 15:01:48 +0300

    implemented Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,75 @@
-import { TopBar, SideMenu, Container } from "components";
-import "./Layout.scss";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Layout = () => {
+import { Icon, Layout, Sidebar } from "ebs-design";
+
+import { UserContext } from "context/UserContext";
+// import { TopBar, SideMenu, Container } from "components";
+import "./Layout.scss";
+import { UserAvatar } from "components/UserAvatar";
+import { Button, Container } from "components";
+
+const MyLayout = () => {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
   return (
-    <div className="layout">
-      <SideMenu />
-      <div className="layout__container">
-        <TopBar />
+    <Layout>
+      <Layout.Topbar>
+        <Layout.Topbar.Toggler />
+        <Layout.Topbar.Title>Metrix</Layout.Topbar.Title>
+        <Layout.Topbar.RightSide>
+          <UserAvatar name={user!.name} surname={user!.surname} />
+          <span>
+            {user!.name} {user!.surname}
+          </span>
+          <Button type="primary" color="danger" onClick={() => {}}>
+            {/* <Icon type="edit" /> */}
+            Log Out
+          </Button>
+        </Layout.Topbar.RightSide>
+      </Layout.Topbar>
+      <Sidebar>
+        {/* <Sidebar.TopMenu> */}
+        <Sidebar.Item
+          onClick={() => navigate("/dashboard")}
+          prefix={
+            <Icon
+              // className=".menu-item__icon"
+              type="apps"
+              style={{ width: "20px", height: "20px" }}
+            />
+          }
+          text="Dashboard"
+        />
+        <Sidebar.Item
+          onClick={() => navigate("/users")}
+          prefix={
+            <Icon
+              // className=".menu-item__icon"
+              type="users"
+              style={{ width: "20px", height: "20px" }}
+            />
+          }
+          text="Users"
+        />
+        <Sidebar.Item
+          onClick={() => navigate("/posts")}
+          prefix={
+            <Icon
+              // className=".menu-item__icon"
+              type="edit"
+              style={{ width: "20px", height: "20px" }}
+            />
+          }
+          text="Posts"
+        />
+      </Sidebar>
+      <Layout.Content style={{ height: "100%", paddingBottom: "0px" }}>
         <Container />
-      </div>
-    </div>
+      </Layout.Content>
+    </Layout>
   );
 };
+
+export { MyLayout as Layout };
